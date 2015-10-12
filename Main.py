0bc4b807d4d9f0d9d98ba77e28a20cf5a1cfa0af
katjan <katjaro@kth.se>
2015-10-12 12:00:41 +0200

    Bigram, trigram probability is done. Just fix the sentenses

--- Main.py
+++ Main.py
@@ -29,63 +29,35 @@ class Main:
 
     nm = NGMaker(stemmedc)
     unigram = nm._setugram(stemmedc)
-    bigramc = nm.makeBigram(stemmedc)
-
-
-
-
+    bigramc = nm._setBigram(stemmedc)
+    trigram = nm._setTrigram(stemmedc)
 
     response = raw_input("Please enter your input: ")
+
+
 
     tmr = TextModifier(response)
     tokenz = tmr.cleanText(response)
 
     #test
-    print('Your input data is:')
-    print(tokenz)
+
 
     #all chars to lowercase
     lowercaseList = tmr.lowercase(tokenz)
-
-    #test
-    print(lowercaseList)
-
     #remove stop words
     noMoreStopwords = tmr.removeStopWords(lowercaseList)
-
-    #test
-    print('No more stop words: ')
-    print(noMoreStopwords)
-
     #Stemming
     stemmed = tmr.stemmText(noMoreStopwords)
 
-    #test
-    print('Stemmed text: ')
+    print('Your input data is:')
     print(stemmed)
 
+    firstWord = stemmed.pop()
 
-
-
-
-    sent = "I am the I am the I am the best computer hacker 4 in, the world and Henrik and Jenny as well. We never knew what would happen"
-
-
-    unigram = sent.split()
-    bigram  = ngrams(sent.split(), bigramN)
-    trigram = ngrams(sent.split(), trigramN)
-
-    fdist1 = FreqDist()
-    fdist2 = FreqDist(bigram)
-
-    for j,v in fdist2.items():
-        print j,v
-
-
-    for word in word_tokenize(sent):
-        fdist1[word.lower()] +=1
-
-    print(fdist1.items())
+    if stemmed in unigram:
+        createSent()
+    else:
+        print('no such word exists')
 
 
 
--- NGMaker.py
+++ NGMaker.py
@@ -2,7 +2,7 @@ from __future__ import division
 #hsd
 from nltk.util import ngrams
 
-from nltk.probability import FreqDist, LidstoneProbDist
+from nltk.probability import FreqDist, SimpleGoodTuringProbDist
 
 class NGMaker():
 
@@ -10,35 +10,88 @@ class NGMaker():
     def __init__( self, listInput ):
         self.text = listInput
 
+    #TODO
     def _setugram(self, listInput):
         self._uniGram = listInput
+
         pdist = self._setuFdist()
-        print (pdist)
+        #print (pdist)
+
+        #hashhash
+
+
         return pdist
 
-    def makeBigram(self, listInput):
-        bigram = ngrams(listInput, 2)
-        #for bigram in bigram:
-            #print (bigram)
-        return bigram
+    def _setBigram(self, listInput):
+        bigramList = ngrams(listInput, 2)
+        bigramFreq = self.countProbability(bigramList)
+        outerBigram = {}
+        size = len(bigramFreq)
 
+        sgtBig = SimpleGoodTuringProbDist(bigramFreq)
+
+
+        for bigram in bigramFreq:
+            b = bigram[0]
+            #print(b)
+            if b in outerBigram :
+                innerBigram1 = outerBigram[b]
+                innerBigram1[b] = (sgtBig.prob(bigram)/size)
+                # print(bigramFreq[bigram]/size)
+            else:
+                innerBigram = {}
+                innerBigram[bigram[1]] = (sgtBig.prob(bigram)/size)
+                outerBigram[b] = innerBigram
+                #print(bigramFreq[bigram]/size)
+
+        return outerBigram
+
+    def _setTrigram(self, listInput):
+        trigramList = ngrams(listInput,3)
+        trigramFreq = self.countProbability(trigramList)
+        outerTrigram = {}
+        size = len(trigramFreq)
+
+        sgtTri = SimpleGoodTuringProbDist(trigramFreq)
+
+        for trigram in trigramFreq:
+            b = trigram[0:2]
+            if b in outerTrigram:
+                innerTrigram = outerTrigram[b]
+                innerTrigram[b] = (sgtTri.prob(trigram)/size)
+            else:
+                innerTrigram = {}
+                #print(trigram[2])
+                innerTrigram[trigram[2]] = (sgtTri.prob(trigram)/size)
+                outerTrigram[b] = innerTrigram
+
+        return outerTrigram
+
+
+    #the function that calls for dist and prob of unigram.
     def _setuFdist(self):
-        self._uniFdist = FreqDist(self._uniGram)
-        return self.makeAProbMap()
-
-
+        self._uniFdist = self.countProbability(self._uniGram)
+        #self._uniFdist = FreqDist(self._uniGram)
+        return self.makeAProbMapUgram()
 
     def makeBigramTree(self, listInput):
         bigram = self.makeBigram(listInput)
 
-
+    #A list vith tuples (word, nr.of occ)
     def countProbability(self, ngram):
         fdist = FreqDist(ngram)
+        return fdist
 
-    def makeAProbMap(self):
+    #makes a dictionary with {word:probability, squirrel:0.343444}
+    def makeAProbMapUgram(self):
         map = {}
         size = len(self._uniFdist)
-        print(size)
         for j,v in self._uniFdist.items():
             map[j] = float(v/size)
         return map
+
+    #makes a dictionary with word as key and a dictionary with common fallowing words as a value
+    def makeAProbMapBigram(self):
+        map = {}
+
+
